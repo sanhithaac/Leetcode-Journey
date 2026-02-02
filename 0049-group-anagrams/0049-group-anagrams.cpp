bf71585f6fac05b74ddb87378f912266d1e080b2
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-02-02 18:38:51 +0530

    Time: 10 ms (92.61%), Space: 25 MB (78.14%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs){
+            string key=s;
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
+        }
+        vector<vector<string>> res;
+        for(auto &p:mp) res.push_back(p.second);
+        return res;
+    }
+};
