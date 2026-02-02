f3ae1f8eec7266abff2d7cb2723a8babcc8147d3
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-02-02 17:38:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+#include <unordered map>
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;        
+            if (freq[num] > 1) { 
+                return true;
+            }
+        }
+
+        return false;
+        
+    }
+};
