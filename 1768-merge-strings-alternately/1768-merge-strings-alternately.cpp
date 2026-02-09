90ba3403b9e206d3e4004f09bb401127f67479e1
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-02-09 19:40:07 +0530

    Time: 0 ms (100%), Space: 8.6 MB (31.71%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result = "";
+        int max_len = max(word1.length(), word2.length());
+
+        for (int i = 0; i < max_len; i++) {
+            if (i < word1.length()) {
+                result += word1[i];
+            }
+            if (i < word2.length()) {
+                result += word2[i];
+            }
+        }
+
+        return result;
+    }
+};
