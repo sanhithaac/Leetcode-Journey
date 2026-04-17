8887669327346ba415f40664614b8246268024a9
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-04-17 22:25:22 +0530

    Time: 0 ms (100%), Space: 24 MB (61.59%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -0,0 +1,16 @@
+class NumArray {
+public:
+    vector<int> prefix;
+
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size() + 1, 0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
