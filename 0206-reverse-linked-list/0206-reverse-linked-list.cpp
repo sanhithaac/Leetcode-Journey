07b28236e1d02396c50c847236e1b80bfaaa6755
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-04-20 22:55:48 +0530

    Time: 0 ms (100%), Space: 13.4 MB (41.2%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        while (head) {
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+};
