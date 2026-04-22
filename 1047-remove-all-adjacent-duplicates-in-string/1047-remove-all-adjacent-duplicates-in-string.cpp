867aabcf6e686540824782da1e169bd517ea63f5
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-04-22 19:38:41 +0530

    Time: 5 ms (59.94%), Space: 13.5 MB (91.03%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string res = "";
+        
+        for(char c : s) {
+            if(!res.empty() && res.back() == c)
+                res.pop_back();
+            else
+                res.push_back(c);
+        }
+        
+        return res;
+    }
+};
