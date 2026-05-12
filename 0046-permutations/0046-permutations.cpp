08b3b2e770bafb27e327adf4c8eeef5b71334367
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-05-12 19:22:39 +0530

    Time: 0 ms (100%), Space: 10.5 MB (68.48%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, vector<int>& curr, vector<vector<int>>& res, vector<bool>& used) {
+        if (curr.size() == nums.size()) {
+            res.push_back(curr);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            curr.push_back(nums[i]);
+
+            backtrack(nums, curr, res, used);
+
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> curr;
+        vector<bool> used(nums.size(), false);
+
+        backtrack(nums, curr, res, used);
+        return res;
+    }
+};
